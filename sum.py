22c1c1673ddb5db9bcd09e7a4f4e8bab39b4cc0e
prateekrastogi92 <root@ip-172-31-86-53.ec2.internal>
2019-06-05 13:21:31 +0000

    used .format

--- sum.py
+++ sum.py
@@ -1,6 +1,4 @@
 x=int(input("enter first number"))
 y=int(input("enter second number"))
 sum_of_numbers = x+y
-print("the sum is: %d" %sum_of_numbers)
- 
-
+print("the sum is: {}" .format(sum_of_numbers))
