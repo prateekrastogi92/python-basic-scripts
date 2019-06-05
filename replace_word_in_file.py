3f4b6fdfeeb59370b7b27268ff459b53797b744e
prateekrastogi92 <root@ip-172-31-86-53.ec2.internal>
2019-06-05 10:10:11 +0000

    updated replace word script

--- replace_word_in_file.py
+++ replace_word_in_file.py
@@ -3,9 +3,10 @@ import sys
 
 import re
 
-text_to_rep = "unix"
-
-with open("replace_word_input.txt", "r+") as file_to_open:
-   new_file = file_to_open.read().replace(text_to_rep, "Linux")
-with open("replace_word_input.txt", "w") as file_to_open:
+text_to_rep = input("Enter text to replace")
+text_after = input("Enter the text to replace with")
+nfile=input("Enter the file name")
+with open(nfile, "r+") as file_to_open:
+   new_file = file_to_open.read().replace(text_to_rep,text_after)
+with open(nfile, "w") as file_to_open:
   file_to_open.write(new_file) 
