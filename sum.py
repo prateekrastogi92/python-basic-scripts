7e4b67931d32461052ac48fe52e56691f3ec3cca
prateekrastogi92 <root@ip-172-31-86-53.ec2.internal>
2019-06-05 10:29:14 +0000

    updated sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
 x=int(input("enter first number"))
 y=int(input("enter second number"))
 sum_of_numbers = x+y
-print ("the sum is:" +str(sum_of_numbers))
+print("the sum is: %d" %sum_of_numbers)
  
 
